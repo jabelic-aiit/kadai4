52bc5845347a53ef72f2ab1d8073c9c143533ab0
Jabelic-aiit <a2239hm@aiit.ac.jp>
2022-08-02 13:39:49 +0000

    ADD: ERROR for decimal

--- gcd-test.sh
+++ gcd-test.sh
@@ -5,20 +5,28 @@ source gcd.sh
 
 assert(){
     expected="$1"
-    input1="$2"
-    input2="$3"
-    # actual=`zsh gcd.sh "$input1" "$input2"`
-    actual=`euclid "$input1" "$input2"`
+    input_1="$2"
+    input_2="$3"
+    actual=`get_gcd "$input_1" "$input_2"`
     if [ "$actual" = "$expected" ]; then
-        echo "$input1, $input2 => $actual"
+        echo "$input_1, $input_2 => $actual"
     else
-        echo "$input1, $input2 => $expected expected, but got $actual"
-        exit 1
+        echo "$input_1, $input_2 => $expected expected, but got $actual"
+        # exit 1
     fi
 }
 
 assert 11 121 33
+assert "ERROR: argument must be a natural number;" 12.1 3.3
+assert "ERROR: argument must be a natural number;" 12.1 33
+assert "ERROR: argument must be a natural number;" 121 3.3
 assert 1 121 31
 assert 6 12 18
 assert 5 25 95
+assert 1 25 "98"
+assert "ERROR: argument must be type number;" "hoge" 98
+assert "ERROR: argument must be type number;" 25 "huga"
+assert "Usage: ARG1 ARG2" 100
+assert 9 999 9999999
+assert 9 999 99999999
 echo "done."
--- gcd.sh
+++ gcd.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/bash
 
+isFullFilledArguments(){
+    # 引数は2つであること
+    if test -z $2 || test -z $3 ;then
+        echo "Usage: ARG1 ARG2";
+        exit 1
+    # 引数は数字であること
+    elif ! [[ $2 =~ [0-9]+ ]] || ! [[ $3 =~ [0-9]+ ]] ;then
+        echo "ERROR: argument must be type number;";
+        exit 1
+    # 引数は自然数であること
+    elif [[ $2 =~ ^-?[1-9]*[0-9](\.[0-9]+)+$ ]] || [[ $3 =~ ^-?[1-9]*[0-9](\.[0-9]+)+$ ]] ;then
+        echo "ERROR: argument must be a natural number;"
+        exit 1
+    fi
+}
+
 euclid(){
     # -lt は <
-
     if test $1 -lt $2 ;then
         large=$2
         small=$1
@@ -18,13 +33,16 @@ euclid(){
             # -lt は <
             if test $(($tmp+$small)) -eq $large || test $tmp -eq $large; then
                 echo ${small}
-                exit 0
+                exit
             elif test $large -lt $(($tmp+$small)); then
                 euclid $small $(($large - $tmp))
             fi
         done
     fi
 }
+get_gcd(){
+    isFullFilledArguments $# $1 $2
+    euclid $1 $2
+}
 
-euclid $1 $2
-export -f euclid
+export -f get_gcd
